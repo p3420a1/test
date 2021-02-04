81d360fecdf62204b14639e9cbb4062b3f68679f
Preeti Agarwal <31091981+p3420a1@users.noreply.github.com>
2021-02-04 12:26:32 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  onPress = (prop) => {
+    this.setState({count: this.state.count + 1}, function(){console.log("State: " + this.state.count)})
+  }
+
+ render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+         style={styles.button}
+         onPress={(num) => this.onPress(num)}
+        >
+         <Text>Click me</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>
+            You clicked { this.state.count } times
+          </Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10
+  }
+})
+
+export default App;
